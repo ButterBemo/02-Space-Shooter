483fb522397e7e8b11aedf1856164ad4de754ed2
Jason Francis <54815962+BL-MSCH-C220-Test@users.noreply.github.com>
2022-02-10 09:11:55 -0500

    Completes demonstration

--- Global.gd
+++ Global.gd
@@ -3,6 +3,28 @@ extends Node
 var VP = Vector2.ZERO
 var score = 0
 var lives = 5
+var level = -1
+
+var levels = [
+	{
+		"title":"Level 1",
+		"subtitle":"Destroy the asteroids",
+		"asteroids":[Vector2(100,100),Vector2(900,500)],
+		"enemies":[],
+		"timer":100,
+		"asteroids_spawned":false,
+		"enemies_spawned":false
+	},
+	{
+		"title":"Level 2",
+		"subtitle":"Destroy the asteroids and watch out for the enemy",
+		"asteroids":[Vector2(100,100),Vector2(900,500),Vector2(800,200)],
+		"enemies":[Vector2(150,500)],
+		"timer":80,
+		"asteroids_spawned":false,
+		"enemies_spawned":false
+	}
+]
 
 
 func _ready():
@@ -11,10 +33,22 @@ func _ready():
 	VP = get_viewport().size
 	var _signal = get_tree().get_root().connect("size_changed", self, "_resize")
 	reset()
-	
+
+func _physics_process(_delta):
+	var A = get_node_or_null("/root/Game/Asteroid_Container")
+	var E = get_node_or_null("/root/Game/Enemy_Container")
+	if A != null and E != null:
+		var L = levels[level]
+		if L["asteroids_spawned"] and A.get_child_count() == 0 and L["enemies_spawned"] and E.get_child_count() == 0:
+			next_level()
+
 func reset():
 	score = 0
 	lives = 5
+	level = -1
+	for l in levels:
+		l["asteroids_spawned"] = false
+		l["enemies_spawned"] = false
 
 func _unhandled_input(_event):
 	if Input.is_action_pressed("quit"):
@@ -37,3 +71,12 @@ func update_lives(l):
 	var Lives = get_node_or_null("/root/Game/UI/HUD/Lives")
 	if Lives != null:
 		Lives.text = "Lives: " + str(lives)
+
+func next_level():
+	level += 1
+	if level >= levels.size():
+		var _scene = get_tree().change_scene("res://UI/End_Game.tscn")
+	else:
+		var Level_Label = get_node_or_null("/root/Game/UI/Level")
+		if Level_Label != null:
+			Level_Label.show_labels()
--- UI/Game.gd
+++ UI/Game.gd
@@ -3,3 +3,4 @@ extends Node2D
 func _ready():
 	Global.update_lives(0)
 	Global.update_score(0)
+	Global.next_level()
