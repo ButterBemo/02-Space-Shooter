875eb46738ba66fdd0130be19ecd5187863a1efc
Jason Francis <kokaubeam2@yahoo.com>
2022-02-15 08:00:33 -0500

    Completes demo

--- Global.gd
+++ Global.gd
@@ -1,8 +1,13 @@
 extends Node
 
+const SCORES = "user://scores.sav"
+const SECRET = "I LOVE PROJECT 02"
+
 var VP = Vector2.ZERO
 var score = 0
 var lives = 5
+
+var scores = []
 
 
 func _ready():
@@ -15,6 +20,7 @@ func _ready():
 func reset():
 	score = 0
 	lives = 5
+	load_scores()
 
 func _unhandled_input(_event):
 	if Input.is_action_pressed("quit"):
@@ -37,3 +43,39 @@ func update_lives(l):
 	var Lives = get_node_or_null("/root/Game/UI/HUD/Lives")
 	if Lives != null:
 		Lives.text = "Lives: " + str(lives)
+
+func add_score():
+	var temp = []
+	var trailer = 10000000000
+	var added = false
+	for s in scores:
+		if score < trailer and score > s["score"]:
+			temp.append({"score":score, "date":OS.get_datetime()})
+			added = true
+		temp.append(s)
+		trailer = s["score"]
+	if not added:
+		temp.append({"score":score, "date":OS.get_datetime()})
+	scores = temp
+	save_scores()
+	
+	
+
+func load_scores():
+	var save_scores = File.new()
+	if not save_scores.file_exists(SCORES):
+		return
+	
+	save_scores.open_encrypted_with_pass(SCORES, File.READ, SECRET)
+	var contents = save_scores.get_as_text()
+	var json_contents = JSON.parse(contents)
+	if json_contents.error == OK:
+		scores = json_contents.result
+	save_scores.close()
+
+func save_scores():
+	var save_scores = File.new()
+	save_scores.open_encrypted_with_pass(SCORES, File.WRITE, SECRET)
+	save_scores.store_string(to_json(scores))
+	save_scores.close()
+
--- UI/End_Game.gd
+++ UI/End_Game.gd
@@ -1,7 +1,14 @@
 extends Control
 
 func _ready():
+	Global.add_score()
 	$Label.text = "Thanks for playing! Your score was " + str(Global.score) + "."
+	$Scores.text = "High Scores:\n"
+	var count = 0
+	for score in Global.scores:
+		if count < 5:
+			$Scores.text = $Scores.text + str(score["score"]) + "\n"
+			count += 1
 
 
 func _on_Play_pressed():
