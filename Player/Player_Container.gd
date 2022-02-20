916f79f53600e4eb9185848f1e6aee134eb5ad33
Jason Francis <54815962+BL-MSCH-C220-Test@users.noreply.github.com>
2022-02-19 23:29:51 -0500

    Completes demonstration

--- Player/Player_Container.gd
+++ Player/Player_Container.gd
@@ -2,8 +2,25 @@ extends Node2D
 
 onready var Player = load("res://Player/Player1.tscn")
 onready var Player2 = load("res://Player/Player2.tscn")
+var spawning = false
+
+func _ready():
+	spawn()
 
 func _physics_process(_delta):
+	var p1 = get_node_or_null("Player")
+	var p2 = get_node_or_null("Player2")
+	if (p1 == null or p2 == null) and not spawning:
+		var Respawn = get_node_or_null("/root/Game/Respawn")
+		if Respawn != null:
+			spawning = true
+			Respawn.start()
+
+
+func _on_Respawn_timeout():
+	spawn()
+
+func spawn():
 	var p1 = get_node_or_null("Player")
 	var p2 = get_node_or_null("Player2")
 	if p1 == null:
@@ -14,3 +31,5 @@ func _physics_process(_delta):
 		var player2 = Player2.instance()
 		player2.position = Vector2(3*Global.VP.x/4, Global.VP.y/2)
 		add_child(player2)
+	spawning = false
+	
--- UI/Camera.gd
+++ UI/Camera.gd
@@ -1,7 +1,7 @@
 extends Camera2D
 
-func _ready():
-	pass
+var transition = false
+var tweened = false
 
 func _physics_process(_delta):
 	var Player1 = get_node_or_null("/root/Game/Player_Container/Player")	
@@ -10,8 +10,32 @@ func _physics_process(_delta):
 		var p1 = Player1.global_position
 		var p2 = Player2.global_position
 		var c = (p1 + p2) / 2
-		global_position = c
 		var vp_size = get_viewport().size.length()
 		var p_dist = p1 - p2
 		var z = clamp(p_dist.length()*1.8/vp_size,0.5,3)
-		zoom = Vector2(z,z)
+		if transition and not $Tween.is_active():
+			$Tween.interpolate_property(self, "global_position", global_position, c, 1.0, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+			$Tween.interpolate_property(self, "zoom", zoom, Vector2(z,z), Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+			$Tween.start()
+			transition = false
+		elif not $Tween.is_active():
+			global_position = c
+			zoom = Vector2(z,z)
+			tweened = false
+	elif not $Tween.is_active() and not tweened:
+		var pos = Vector2.ZERO
+		if Player1 != null:
+			pos = Player1.global_position
+		if Player2 != null:
+			pos = Player2.global_position
+		if pos != Vector2.ZERO:
+			$Tween.interpolate_property(self, "global_position", global_position, pos, 1.0, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		$Tween.interpolate_property(self, "zoom", zoom, Vector2(1,1), Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		$Tween.start()
+		transition = true
+		tweened = true
+	else:
+		if Player1 != null:
+			global_position = Player1.global_position
+		if Player2 != null:
+			global_position = Player2.global_position
